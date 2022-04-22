ca5c0b29bdbcd0e7212b28702c04aa0ebc86ce5c
Agrippino <agrippinorussiello@outlook.it>
2022-04-22 19:04:02 +0200

    Errore mio, ho notato che non ero riuscito a pushare per 18, se non verrà contato il programma lo capisco, questo è tutto quello che ho fatto fino alle 18.

---
+++ GestoreEventi/Evento.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestoreEventi
+{
+    internal class Evento
+        //variabili//
+    {
+        private protected string Titolo { get; set; }
+       
+        private protected int numeroPostiTotali;
+
+        private protected int numeroPostiPrenotati;
+
+        private protected int numeroPostiRimanenti;
+
+        private protected int postiDaRimuovere;
+
+        private protected DateTime dataEvento = new DateTime();
+
+        private protected DateTime dataDiOggi =  DateTime.Now;
+        //costruttore//
+        public Evento (string Titolo, int numeroPostiTotali, DateTime dataEvento)
+        {
+            this.Titolo = Titolo;
+            this.numeroPostiTotali = numeroPostiTotali;
+            this.dataEvento = dataEvento;
+
+            //controlli per i parametri //
+            if (Titolo == "")
+            {
+                throw new Exception("Mi dispiace non hai inserto nulla, per favore inserisci un titolo");
+            }
+            if (dataEvento < dataDiOggi)
+            {
+                throw new Exception("Mi dispiace la data inserita non è corretta, reinserisci la data.");
+            }
+            if(numeroPostiTotali< 0 || numeroPostiTotali < 1)
+            {
+                throw new Exception("Mi dispiace non puoi inserire un numero minore di 0 o inferiore a 1, per favore inserisci un numero divero");
+            }
+
+        }
+
+      
+
+        // sezione metodi e get-set//
+
+        // Metodo che si occupa di cambiare il titolo
+        public void RinominaTitolo(string nuovoTitolo)
+        {
+            Titolo = nuovoTitolo;
+        }
+        // Metodo che si occupa di cambiare la data
+        public void CambiaData(DateTime nuovaData)
+        {
+            dataEvento = nuovaData;
+        }
+
+
+
+        //metodi per prenotare i posti //
+
+        public void PrenotaPosti(int postiPrenotati)
+        {
+            // verifica se hai abbastanza post liberi
+            // se la data dell'evento non è già passata
+
+            this.numeroPostiPrenotati = this.numeroPostiPrenotati + postiPrenotati;
+            this.numeroPostiRimanenti = this.numeroPostiTotali - this.numeroPostiPrenotati;
+
+            if (dataEvento < dataDiOggi)
+            {
+                Console.WriteLine("Mi dispiace questo evento non è più disponibile");
+            }
+            if (this.numeroPostiPrenotati > this.numeroPostiRimanenti || this.numeroPostiRimanenti < 0 )
+            {
+                Console.WriteLine("Mi dispaice non sono disponibili tutti questi posti");
+            }
+            else
+            {
+                Console.WriteLine("Grazie per la sua prenotazione");
+                Console.WriteLine("I posti rimanenti ora sono: " + numeroPostiRimanenti);
+            } 
+        }
+
+        //metodo per disidire i posti//
+        public void DisdiciPosti(int disdiciPosti)
+        {
+            this.postiDaRimuovere = this.numeroPostiPrenotati - disdiciPosti;
+            this.numeroPostiRimanenti = this.numeroPostiTotali - this.postiDaRimuovere;
+
+            if (numeroPostiPrenotati < disdiciPosti)
+            {
+                Console.WriteLine("Mi dispiace non ci sono più posti da rimuovere ");
+            }
+            if (dataEvento < dataDiOggi)
+            {
+                Console.WriteLine("Mi dispiace questo evento non è più disponibile");
+
+            }
+            else
+            {
+                Console.WriteLine("I posti disdetti sono: " + postiDaRimuovere + " , grazie e arrivederci.");
+                Console.WriteLine("I posti rimanenti sono: " + numeroPostiRimanenti);
+            }
+
+        }
+
+        //metodo per chiedere più volte di disdire
+        public void RichiestaPerDisdire(string rispostaRichiesta)
+        {
+            bool controlloRichiesta = false;
+            while (controlloRichiesta == false)
+            {   string casoRisposta= Console.ReadLine();
+                switch (casoRisposta)
+                { 
+                    case "Si" :
+
+                        Console.WriteLine("Quanti posti vuole disdire per questo eveto? Attualmente sono prenotati " + numeroPostiTotali + " posti.");
+                        int disdiciPrenotazioni = int.Parse(Console.ReadLine());
+                        DisdiciPosti(disdiciPrenotazioni);
+
+                        Console.WriteLine("Numero posti prenotati: " + numeroPostiPrenotati );
+                        Console.WriteLine("Numero posti disponibili: " +  numeroPostiRimanenti);
+                        break;
+                    case "No" :
+                        Console.WriteLine("Grazie mille, arrivederci");
+                        controlloRichiesta = true;
+                        break;
+                    default:
+                    Console.WriteLine("Scrivi qualcosa per favore.Grazie.");
+                        break;
+                }
+            }
+        }
+    //metodo per stampare (override del metodo generale ToString())//
+
+    public virtual void ToString()
+         {
+            Console.WriteLine("---Eventi--- \n");
+            Console.WriteLine("Data e titolo dell'evento \n");
+            Console.WriteLine(this.dataEvento + " " + this.Titolo);
+            Console.WriteLine("Il numero di posti totali sono: " + this.numeroPostiTotali );
+            Console.WriteLine("I numeri di posti prenotati è " + this.numeroPostiPrenotati);
+            Console.WriteLine("I numeri di posti rimanenti è " + this.numeroPostiRimanenti);
+
+        }
+    }
+}
--- GestoreEventi/Program.cs
+++ GestoreEventi/Program.cs
@@ -1,2 +1,92 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using GestoreEventi;
+//creo un costruttore//
+Console.WriteLine("Titolo: ");
+string Titolo = Console.ReadLine();
+Console.WriteLine("numero posti totali: ");
+int  numeroPostiTotali=int.Parse(Console.ReadLine());
+Console.WriteLine("data evento: ");
+DateTime dataEvento = DateTime.Parse(Console.ReadLine());
+
+
+
+
+Evento nuovoEventoInProgramma = new Evento(Titolo,numeroPostiTotali,dataEvento) ;
+
+nuovoEventoInProgramma.ToString();
+
+Console.WriteLine("Qual è il nuovo titolo dell'evento? Attualmente il titolo è: " + Titolo);
+string nuovoTitolo = Console.ReadLine() ;
+nuovoEventoInProgramma.RinominaTitolo(nuovoTitolo);
+
+nuovoEventoInProgramma.ToString();
+
+Console.WriteLine("Qual è la nuova data dell'evento? La data dell'evento attuale è : " + dataEvento);
+DateTime nuovaData = DateTime.Parse(Console.ReadLine());
+nuovoEventoInProgramma.CambiaData(nuovaData);
+
+nuovoEventoInProgramma.ToString();
+
+
+Console.WriteLine("Quanti posti vuole prenotare per questo eveto? Attualmente sono disponibili " + numeroPostiTotali + " posti.");
+int postiPrenotati = int.Parse(Console.ReadLine());
+nuovoEventoInProgramma.PrenotaPosti(postiPrenotati);   
+
+nuovoEventoInProgramma.ToString();
+
+Console.WriteLine("Vuole disdire dei posti per questo evento ? [Si/No] ");
+string richiestaPosti = Console.ReadLine();
+nuovoEventoInProgramma.RichiestaPerDisdire(richiestaPosti);
+
+nuovoEventoInProgramma.ToString();
+
+
+
+//creo una lista//
+/*List<Evento> listaDegliEventi = new List<Evento>();
+
+Console.WriteLine("Quanti eventi devi programmare?");
+int numeroDiEventiInProgramma = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < numeroDiEventiInProgramma; i++)
+{
+    Console.WriteLine("Ciao sono il tuo aiutante per programmare gli eventi, ho visto che hai inserito " + numeroDiEventiInProgramma + " eventi \n ");
+    Console.WriteLine("Qual è il nome dell'evento ? \n");
+    string nome = Console.ReadLine();
+
+    //creo una varaibile booleana da inserire in un ciclo while per controllare la data inserita//
+    bool controlloData = false;
+    while (controlloData == false)
+    {
+        try
+        {
+
+            Console.WriteLine("Inserisci la data dell'evento \n ");
+            DateTime data = DateTime.Parse(Console.ReadLine());
+
+            controlloData = true;
+        }
+        catch (Exception erroreData)
+        {
+            Console.WriteLine(erroreData.Message);
+        }
+    }
+    //creo una variabile booleana da insrire in un ciclo while per controllare il numero di posti inseriti//
+    bool controlloNumeroPosti = false;
+    while (controlloNumeroPosti == false) ;
+    {
+        try 
+        {
+            Console.WriteLine("Inserisci il numero di posti totali");
+            int numeroDiPosti = int.Parse(Console.ReadLine());
+
+            controlloNumeroPosti= true;
+        } catch (Exception erroreNumeroPosti)
+        {
+            Console.WriteLine(erroreNumeroPosti.Message);
+        }
+    }
+
+
+   
+}*/
+
